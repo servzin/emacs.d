d215f30dc74719a84fd8e601dbecec4f9b84b7a6
servzin <servzin@gmail.com>
2014-10-22 12:05:40 +0400

    Add js autoindent and remove auto-fill mode

--- init.el
+++ init.el
@@ -18,8 +18,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
- '(custom-enabled-themes (quote (pastels-on-dark soothe tango-dark)))
- '(custom-safe-themes (quote ("4c9ba94db23a0a3dea88ee80f41d9478c151b07cb6640b33bfc38be7c2415cc4" "30fe7e72186c728bd7c3e1b8d67bc10b846119c45a0f35c972ed427c45bacc19" "787574e2eb71953390ed2fb65c3831849a195fd32dfdd94b8b623c04c7f753f0" "68769179097d800e415631967544f8b2001dae07972939446e21438b1010748c" default)))
+ '(custom-enabled-themes (quote (ample soothe tango-dark)))
+ '(custom-safe-themes (quote ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1dc3a2e894d5ee9e90035e4ff90d57507857c07e9a394f182a961e935b3b5497" "5e6c2e2116c7a72ae0668390f92504fd0b77524cedd387582648b1aa1c582f59" "4c9ba94db23a0a3dea88ee80f41d9478c151b07cb6640b33bfc38be7c2415cc4" "30fe7e72186c728bd7c3e1b8d67bc10b846119c45a0f35c972ed427c45bacc19" "787574e2eb71953390ed2fb65c3831849a195fd32dfdd94b8b623c04c7f753f0" "68769179097d800e415631967544f8b2001dae07972939446e21438b1010748c" default)))
  '(vc-annotate-background "#2b2b2b")
  '(vc-annotate-color-map (quote ((20 . "#bc8383") (40 . "#cc9393") (60 . "#dfaf8f") (80 . "#d0bf8f") (100 . "#e0cf9f") (120 . "#f0dfaf") (140 . "#5f7f5f") (160 . "#7f9f7f") (180 . "#8fb28f") (200 . "#9fc59f") (220 . "#afd8af") (240 . "#bfebbf") (260 . "#93e0e3") (280 . "#6ca0a3") (300 . "#7cb8bb") (320 . "#8cd0d3") (340 . "#94bff3") (360 . "#dc8cc3"))))
  '(vc-annotate-very-old-color "#dc8cc3"))
@@ -42,7 +42,51 @@
       (comment-or-uncomment-region (line-beginning-position) (line-end-position))
     (comment-dwim arg)))
 (global-set-key "\M-;" 'comment-dwim-line)
+;; Ruby end
 (require 'ruby-mode)
 (defun turn-on-end-mode () (ruby-end-mode 1))
 (add-hook 'ruby-mode-hook 'turn-on-end-mode)
+;; Tramp
 (setq tramp-default-method "ssh")
+;; Javascript 
+(add-hook 'js-mode-hook 'js2-minor-mode)
+(setq js-basic-indent 2)
+(setq-default js2-basic-indent 2)
+
+(setq-default js2-basic-offset 2)
+(setq-default js2-auto-indent-p t)
+(setq-default js2-cleanup-whitespace t)
+(setq-default js2-enter-indents-newline t)
+(setq-default js2-global-externs "jQuery $")
+(setq-default js2-indent-on-enter-key t)
+(setq-default js2-mode-indent-ignore-first-tab t)
+
+(setq-default js2-global-externs '("module" "require" "buster" "sinon" "assert" "refute" "setTimeout" "clearTimeout" "setInterval" "clearInterval" "location" "__dirname" "console" "JSON"))
+
+;; We'll let fly do the error parsing...
+(setq-default js2-show-parse-errors nil)
+
+(autoload 'js2-mode "js2-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(font-lock-add-keywords
+ 'js2-mode `(("\\(function *\\)("
+             (0 (progn (compose-region (match-beginning 1) (match-end 1) "ƒ")
+                       nil)))))
+(font-lock-add-keywords 'js2-mode
+                        '(("\\<\\(FIX\\|TODO\\|FIXME\\|HACK\\|REFACTOR\\):"
+                           1 font-lock-warning-face t)))
+
+(defun add-indent-hook (mode)
+  (add-hook mode
+            '(lambda ()
+               (local-set-key (kbd "RET") 'reindent-then-newline-and-indent))))
+
+(mapcar 'add-indent-hook '(lisp-mode-hook html-mode-hook
+                           ruby-mode-hook js2-mode-hook
+                           js-mode-hook
+                           c++-mode-hook d-mode-hook
+                           scheme-mode-hook c-mode-hook
+                           emacs-lisp-mode-hook scala-mode-hook
+                           clojure-mode-hook))
+(remove-hook 'prog-mode-hook 'esk-local-comment-auto-fill)
+(remove-hook 'text-mode-hook 'turn-on-auto-fill)
